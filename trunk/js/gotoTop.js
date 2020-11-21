10cca5b8686c048f49524a76cb58ca4e949ed33c
xxldyy <1184159314@qq.com>
2020-11-21 21:00:06 +0800

    这是第六周的项目内容文件

---
+++ trunk/js/gotoTop.js
@@ -0,0 +1,40 @@
+/*返回顶部功能函数封装*/
+
+//当页面加载完成
+$(function(){
+    //把函数挂在window上暴露出去
+     window.gotoTop =function(options){
+        var $gotoTopHtml= $(`<div class="backToTop"><img src="${options.imgUrl}"></div>`);
+
+        //写样式定位
+        $gotoTopHtml.css({
+            width: '30px',
+            height: '50px',
+            position: 'fixed',
+            bottom: '100px',
+            left:'710px',
+            marginLeft: '50%',
+            /*默认隐藏*/
+            display: 'none',
+        });
+
+        //返回顶部js代码
+        $(document).scroll(function(){
+            var topDistance=$('html,body').scrollTop();
+            if(topDistance>500){
+                $('.backToTop').fadeIn();
+            }else{
+                $('.backToTop').fadeOut();
+            }
+        })
+        $('body').on('click','.backToTop',function(){
+            $('html,body').animate({
+                scrollTop:0
+            },300)
+        })
+        //追加到页面尾部
+       $('body').append($gotoTopHtml)
+    }
+    //准备结构
+    
+})
--- trunk/js/index.js
+++ trunk/js/index.js
@@ -1,74 +1,131 @@
 /**
  * 
  * 乐购商城首页JS
- * 2020-10-28
+ * 2020-10-22 by zcw
  */
 //当页面加载完毕
 $(function(){
 $('#banner').tyslide({
     boxh:460,//盒子的高度
-    w:1000,//盒子的宽度
+    w:1150,//盒子的宽度
     h:390,//图片的高度
     isShow:true,//是否显示控制器
     isShowBtn:true,//是否显示左右按钮
     controltop:40,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
-    controlsW:20,//控制按钮宽度
-    controlsH:20,//控制按钮高度
+    controlsW:25,//控制按钮宽度
+    controlsH:25,//控制按钮高度
     radius:10,//控制按钮圆角度数
     controlsColor:"#d7d7d7",//普通控制按钮的颜色
     controlsCurrentColor:"#ff6600",//当前控制按钮的颜色
     isShowNum:true //是否显示数字
 });
-/*图书电子书小轮播 */
+
+
+/*图书 电子书轮播 */
+$(function(){
     $('#ebooks-banner').tyslide({
         boxh:223,//盒子的高度
-        w:332,//盒子的宽度
+        w:380,//盒子的宽度
         h:223,//图片的高度
         isShow:true,//是否显示控制器
         isShowBtn:true,//是否显示左右按钮
-        controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controltop:5,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
         controlsW:20,//控制按钮宽度
-        controlsH:3,//控制按钮高度
+        controlsH:4,//控制按钮高
         controlsColor:"#d7d7d7",//普通控制按钮的颜色
-        controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
+        controlsCurrentColor:"white",//当前控制按钮的颜色
+    
     });
+    })
+
+/*电子书 table切换 */
+var $lis = $('.ebooks .ebooks-nav > li');
+$lis .mouseover(function(){
+    //给自己添加激活类active 把兄弟的都干掉
+    $(this).addClass('active').siblings().removeClass('active');
+    //获取index
+    var index = $(this).index();
+
+    //选中内容
+    var $ebooksList = $('ebooks-list');
+    //显示索引等于当前Index
+    $ebooksList.eq(index).show().siblings('.ebooks-list').hide();
+})
 
 
-        /*新书列表手风琴效果 */
-        $('.ebooks .right-box ul > li').mouseenter(function(){
-            //所有兄弟：隐藏详情 显示标题
-            $(this).siblings().find('.desc').slideUp();
-            $(this).siblings().find('.ebooks-title').slideDown();
+/*新书列表交互效果*/
+$('.ebooks .right-box ul > li').mouseenter(function () {
+    //所有兄弟:隐藏详情 显示标题
+    $(this).siblings().find('.desc').hide();//隐藏详情
+    $(this).siblings().find('.ebooks-title').show();//显示标题
 
-            //当前：隐藏标题 显示详情
-            $(this).find('.ebooks-title').slideUp(); //隐藏标题
-            $(this).find('.desc').slideDown(); //显示详情
+//当前：隐藏标题 显示详情
+$(this).find('.ebooks-title').hide();//当前：隐藏标题
+$(this).find('.desc').show();//当前：显示详情
+})
+})
+
+/*女装图轮播 */
+$(function(){
+    $('#clothes-banner').tyslide({
+        boxh:340,//盒子的高度
+        w:427,//盒子的宽度
+        h:340,//图片的高度
+        isShow:true,//是否显示控制器
+        isShowBtn:true,//是否显示左右按钮
+        controltop:5,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW:25,//控制按钮宽度
+        controlsH:4,//控制按钮高
+        controlsColor:"gray",//普通控制按钮的颜色
+        controlsCurrentColor:"white",//当前控制按钮的颜色
+    
+    });
+    })
+
+    /*运动图轮播 */
+$(function(){
+    $('#sport-banner').tyslide({
+        boxh:340,//盒子的高度
+        w:427,//盒子的宽度
+        h:340,//图片的高度
+        isShow:true,//是否显示控制器
+        isShowBtn:true,//是否显示左右按钮
+        controltop:5,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW:25,//控制按钮宽度
+        controlsH:4,//控制按钮高
+        controlsColor:"gray",//普通控制按钮的颜色
+        controlsCurrentColor:"white",//当前控制按钮的颜色
+    
+    });
+    })
+
+        /*童装图轮播 */
+$(function(){
+    $('#kid-banner').tyslide({
+        boxh:340,//盒子的高度
+        w:427,//盒子的宽度
+        h:340,//图片的高度
+        isShow:true,//是否显示控制器
+        isShowBtn:true,//是否显示左右按钮
+        controltop:5,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW:25,//控制按钮宽度
+        controlsH:4,//控制按钮高
+        controlsColor:"gray",//普通控制按钮的颜色
+        controlsCurrentColor:"white",//当前控制按钮的颜色
+    
+    });
+    })
+        /* 推广商品切换 */
+
+        $('.tg .promotion-title ul li').mouseenter(function(){
+            //导航激活类的切换
+            $(this).addClass('active').siblings().removeClass('active')
+            //内容切换
+            //获取对应索引
+            var index = $(this).index();
+            $('.tg .promotion-content .inner-box').animate({
+                'left' : -index * 1170
+            })
         })
 
     
-})/*服装小轮播*/
-$('#clothes-banner').tyslide({
-    boxh:2800,//盒子的高度
-    w:565,//盒子的宽度
-    h:328,//图片的高度
-    isShow:true,//是否显示控制器
-    isShowBtn:true,//是否显示左右按钮
-    controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
-    controlsW:20,//控制按钮宽度
-    controlsH:3,//控制按钮高度
-    controlsColor:"#d7d7d7",//普通控制按钮的颜色
-    controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
-});
-/*户外运动小轮播*/
-$('#sport-banner').tyslide({
-    boxh:1000,//盒子的高度
-    w:515,//盒子的宽度
-    h:520,//图片的高度
-    isShow:true,//是否显示控制器
-    isShowBtn:true,//是否显示左右按钮
-    controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
-    controlsW:20,//控制按钮宽度
-    controlsH:3,//控制按钮高度
-    controlsColor:"#d7d7d7",//普通控制按钮的颜色
-    controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
-});
