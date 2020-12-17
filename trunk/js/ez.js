92fb4df01fc58988d755b94e6393ed9b1056ae6a
xxldyy <1184159314@qq.com>
2020-12-17 20:16:18 +0800

    这是第十周的项目内容文件

--- trunk/js/ez.js
+++ trunk/js/ez.js
@@ -33,4 +33,48 @@
          //显示对应的内容
          $('.ez-banner').eq(index).show().siblings().hide();
     })
+
+
+   /*换一批 */
+   //定一个索引
+   //上下滑动版本
+    //var index=0;
+
+   //$('.change').click(function(){
+       //自增
+       //index++;
+
+      //边界判断
+      //index=index>2?0:index;
+       //让里面的inner-box运动
+      // $('.inner-box').animate({
+       //  top:-index*500
+       //})
+
+   //})
+
+   //左右滑动版本
+  var index=0;
+
+   $('.change').click(function(){
+       //自增
+       index++;
+
+      //边界判断
+      index=index>3?0:index;
+       //让里面的inner-box运动
+       $('.inner-box').stop(true).animate({left:-index*1200},function(){
+          if(index ===3){
+             index =0;
+             $('.inner-box').css('left',0);
+          }
+       })
+
+
+  })
+
    })
+
+
+
+
